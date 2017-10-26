53c4bbd9c498f026804ca6c9d1f68c54e58ec3ff
browe56 <rowebrittany@gmail.com>
2017-10-26 14:52:37 -0600

    Add project files.

---
+++ CustomerConsoleApplication/Customer.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerConsoleApplication
+{
+    //Customer Class Definition
+    class Customer
+    {
+        //Class Variables Go Here
+        private static int versionNumber;
+
+        //Instance Variables Go Here
+        private string customerName;
+        private string customerAddress;
+        private int customerZip;
+
+        //Constructor Method Goes Here
+        public Customer()
+        {
+            Console.WriteLine("Customer Object Created!");
+        }
+        //Class Methods Go Here
+        public static int GetVersionNumber()
+        {
+            return versionNumber;
+        }//end method
+
+        public static void setVersionNumber(int newVersionNumber)
+        {
+            versionNumber = newVersionNumber;
+        }//end method
+
+        //Instance Methods Go Here
+
+        //retrieve the customer name
+        public string getCustomerName()
+        {
+            return customerName;
+        }//end of getCustomerName method
+
+        //set the customer name
+        public void setCustomerName(string newName)
+        {
+            customerName = newName;
+        }//end of setCustomerName method
+
+        //retrieve the customer address
+        public string getCustomerAddress()
+        {
+            return customerAddress;
+        }//end of getCustomerAddress method
+
+        //set the customer address
+        public void setCustomerAddress(string newAddress)
+        {
+            customerAddress = newAddress;
+        }//end of setCustomerAddress
+
+        //retrieve the customer zip
+        public int getCustomerZip()
+        {
+            return customerZip;
+        }//end of getCustomerZip method
+
+        //set the customer zip
+        public void setCustomerZip(int newZip)
+        {
+            customerZip = newZip;
+        }//end of setCustomerZip method
+
+
+    }//end of Customer Class definition
+
+}//end of namespace definition
---
+++ CustomerConsoleApplication/CustomerTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Customer console application
+namespace CustomerConsoleApplication
+{
+    class CustomerTest
+    {
+        //main method
+        static void Main(string[] args)
+        {
+            //Create a Customer Object
+            Customer myCustObj = new Customer();
+
+            //set Customer version number
+            Customer.setVersionNumber(1); //set versionNo to 1
+
+            //display the Customer version number
+            Console.WriteLine(); //display a blank line
+            Console.WriteLine("Customer Version Number: " + Customer.GetVersionNumber());
+
+            //set the customer instance variables
+            myCustObj.setCustomerName("Rick Birney");
+            myCustObj.setCustomerAddress("12 West Street");
+            myCustObj.setCustomerZip(12345);
+
+            //display the entered customer information
+            Console.WriteLine("Customer Name: " + myCustObj.getCustomerName());
+            Console.WriteLine("Customer Address: " + myCustObj.getCustomerAddress());
+            Console.WriteLine("Customer Zip: " + myCustObj.getCustomerZip());
+            
+            //Pause DOS Display Window
+            Console.WriteLine();
+            Console.WriteLine("Press any key to Exit.");
+            Console.ReadLine();
+
+        }//end main method
+
+    } //end CustomerTest class definition
+} //end namespace
